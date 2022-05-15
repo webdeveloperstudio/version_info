0547c9dd808d041c2e082dad06459cb1c4483a1d
MerabC <merabchik83@gmail.com>
2022-05-16 00:51:10 +0400

    Add interfaces

---
+++ src/interfaces/app.interface.ts
@@ -0,0 +1,11 @@
+export default interface AppInterface {
+    name: string;
+    version: string;
+    description: string;
+    url: string;
+    icon: string;
+    autor_name: string;
+    autor_email: string;
+    autor_website: string;
+    autor_github: string;
+}
---
+++ src/interfaces/autor.interface.ts
@@ -0,0 +1,6 @@
+export default interface AutorInterface {
+    name: string;
+    email: string;
+    website: string;
+    github: string;
+}
